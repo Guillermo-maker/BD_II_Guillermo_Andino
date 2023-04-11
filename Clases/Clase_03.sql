65e95153f326512e2f9d1b753f2889b0d1bd15e2
Guillermo-maker <gustavoandino2@gmail.com>
2023-04-11 11:43:14 -0300

    new vertion

--- Clases/Clase_03.sql
+++ Clases/Clase_03.sql
@@ -140,7 +140,7 @@ Select title, rental_rate, replacement_cost from film where replacement_cost bet
 Select title, rating, category_id from film, film_category where special_feactures="Behind the Scenes";
 Select first_name, last_name, film_id from actor, film_actor where film_id=1;
 SELECT country, city, address, store_id FROM country, city, address, store where store_id = 1 ;
-Select title, rating from film where rating='PG-13' limit 2;
+Select f1.title, f1.rating, f2.title, f2.rating from film f1, film f2 where f1.rating = f2.rating and f1.film_id != f2.film_id;
 Select store_id, first_name, last_name from inventory, staff where store_id = 2 ;
 
 
